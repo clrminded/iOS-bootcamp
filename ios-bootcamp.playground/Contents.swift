15d3e9227baf02bfeaefc69620328bbcc113bbce
clrminded <the_rocket_scientist@icloud.com>
2024-07-30 17:29:20 -0700

    Created playground pages for all sections of swift handbook

--- ios-bootcamp.playground/Contents.swift
+++ ios-bootcamp.playground/Contents.swift
@@ -1,4 +1,15 @@
 import UIKit
 
+
 var greeting = "Hello, playground"
+
+
+// declare a variable named welcomeMessage of type string
+var welcomeMessage: String
+// initialize the variable name welcomeMessage with value "Hello, World!"
+welcomeMessage = "Hello, World!"
+
+let pi: Double
+pi = 3.1426
+print(pi)
 
